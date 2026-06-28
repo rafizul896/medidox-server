8ccd3b2763b812a28e986bdb07a8702ca0787e5d
rafizul896 <rafizulislam896@gmail.com>
2026-06-28 16:17:28 +0600

    add: schedulesForDoctor and deleteScheduleFromDB

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -4,5 +4,7 @@ import { ScheduleController } from "./schedule.controller";
 const router = Router();
 
 router.post("/", ScheduleController.createSchedule);
+router.get("/", ScheduleController.schedulesForDoctor);
+router.delete("/:id", ScheduleController.deleteScheduleFromDB);
 
 export const scheduleRoutes = router;
