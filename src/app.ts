d3b8985ef9b6611e324921ef2f75e2daa24f2f64
rafizul896 <rafizulislam896@gmail.com>
2026-07-16 10:25:17 +0600

    cors origin updated

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://medidox-client.vercel.app"],
     credentials: true,
   }),
 );
