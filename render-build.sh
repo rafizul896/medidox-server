8d6c238b06605f75d0a3e1aeb1f200ed70dc25dc
rafizul896 <rafizulislam896@gmail.com>
2026-07-16 08:50:20 +0600

    updated: render-build.sh file

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,31 @@
 set -o errexit
 
-npm install
-npm run build
-npx prisma generate
-npx prisma migrate deploy
+echo "===== Node Version ====="
+node -v
 
+echo "===== NPM Version ====="
+npm -v
+
+echo "===== NODE_ENV ====="
+echo $NODE_ENV
+
+echo "===== Installing Dependencies ====="
+npm install
+
+echo "===== Express ====="
+npm ls express
+
+echo "===== @types/express ====="
+npm ls @types/express
+
+echo "===== TypeScript ====="
+npm ls typescript
+
+echo "===== Building Project ====="
+npm run build
+
+echo "===== Prisma Generate ====="
+npx prisma generate
+
+echo "===== Prisma Migrate ====="
+npx prisma migrate deploy
